59592869c11d9faadaabca4033e25b38bc518d1c
Marcin Kral <marcin.kral@hmail.htog>
2015-06-03 13:54:27 +0200

    Added mock of WebUtility.UrlEncode

---
+++ source/Client.Net35/WebUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Thinktecture.IdentityModel.Client
+{
+    internal static class WebUtility
+    {
+        public static string UrlEncode(string value)
+        {
+            return Uri.EscapeDataString(value).Replace("%20", "+");
+        }
+    }
+}
